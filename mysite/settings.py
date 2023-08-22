7d10416f8767b1b164a7289583dca902265dc62c
irvanshandika <shandikamuhammadirvan@gmail>
2023-08-22 13:29:19 +0700

    Vercel.json 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'blog.apps.BlogConfig',
     'tailwind',
     'theme.apps.ThemeConfig',
     'django_browser_reload'
@@ -121,9 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- theme/views.py
+++ theme/views.py
@@ -3,11 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    first_name = 'Muhammad Irvan'
-    last_name = 'Shandika'
 
-    context = {
-        'first_name': first_name,
-        'last_name': last_name,
-    }
-    return render(request, "base.html", context)
+    return render(request, "base.html")
